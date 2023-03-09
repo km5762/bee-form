0f8dd3c5abdc8dde74d8310d991a4927062e6fd9
km5762 <km5762@DESKTOP-587P2TU>
2023-03-09 16:20:07 -0500

    Fix validation of form to only trigger after invalid form submission

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
 const confirmPassword = document.querySelector('#confirm-password-input');
-
+const form = document.querySelector('form');
+const submit = document.querySelector('#submit-button');
 confirmPassword.addEventListener('change', checkPassword);
+
+submit.addEventListener('click', () => {
+    form.classList.add('submitted');
+})
 
 function checkPassword() {
     const password = document.querySelector('#password-input');
     const confirm = document.querySelector('#confirm-password-input');
-    const confirmPasswordStatus = document.querySelector('#confirm-password-status');
 
     if (password.value === confirm.value) {
         confirm.setCustomValidity('')
